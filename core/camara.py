7db04f49b0fbb6ed8da7a38b59bf943531d19187
Si manglam <90334583+simanglam@users.noreply.github.com>
2023-10-10 17:25:35 +0800

    Rewrote and Restructure the game

--- core/camara.py
+++
@@ -1,9 +0,0 @@
-from pygame.rect import Rect
-
-class Camara:
-    def __init__(self, size: tuple[int, int, int]) -> None:
-        self.rect = Rect(size[0], size[1], size[2], size[2])
-
-    def update(self, x_speed, y_speed):
-        self.rect.x -= x_speed
-        self.rect.y -= y_speed
